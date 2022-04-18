8108cb4165766e9460bfd43a1f5ced96de411690
burggste000 <steedsranch@gmail.com>
2022-04-18 17:13:14 -0600

    removed unnecissary functions and variables that I copied from my msal file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,5 @@
-import React, { useState } from "react";
-import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
-import { loginRequest } from "../../authConfig";
-import { ProfileData } from "../ProfileData";
-import { callMsGraph } from "../../graph";
+import React from "react";
+import { AuthenticatedTemplate, UnauthenticatedTemplate} from "@azure/msal-react";
 import { useIsAuthenticated } from "@azure/msal-react";
 import{LandingPage}from"../LandingPage.jsx";
 import{SignOutButton}from"../SignOutButton";
@@ -11,26 +8,7 @@ import{SteedsTasks}from"../SteedsTasksComponents/SteedsTasks.js";
 //I got all of the code for setting up this entire project from the following website:
 //  https://docs.microsoft.com/en-us/azure/active-directory/develop/tutorial-v2-react
 function App() {
-    const { instance, accounts } = useMsal();
-    const [graphData, setGraphData] = useState(null);
 
-    const name = accounts[0] && accounts[0].name;
-
-    function RequestProfileData() {
-        const request = {
-            ...loginRequest,
-            account: accounts[0]
-        };
-
-        // Silently acquires an access token which is then attached to a request for Microsoft Graph data
-        instance.acquireTokenSilent(request).then((response) => {
-            callMsGraph(response.accessToken).then(response => setGraphData(response));
-        }).catch((e) => {
-            instance.acquireTokenPopup(request).then((response) => {
-                callMsGraph(response.accessToken).then(response => setGraphData(response));
-            });
-        });
-    }
     const isAuthenticated = useIsAuthenticated();
 
     return (
