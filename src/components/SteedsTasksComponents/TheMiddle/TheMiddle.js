b899172030e0953088acb3cb39c7fa7eb93a2537
burggste000 <steedsranch@gmail.com>
2022-04-23 13:00:09 -0600

    added photos for my lists

--- src/components/SteedsTasksComponents/TheMiddle/TheMiddle.js
+++ src/components/SteedsTasksComponents/TheMiddle/TheMiddle.js
@@ -606,8 +606,8 @@ const TheMiddle=(props)=>{
                     <h4 id="listsMenuTasksText">{lists!==null?lists.value[0].displayName:"loading lists"}</h4>
                 </div>
 {/*Working here*/}
-                <div id="listsMenuMyListsDiv">
-                    {lists!==null?lists.value.map((value,index)=>{if(index>0){return(<h4 className="listsMenuMyLists"key={index}>{value.displayName}</h4>);}}):"loading..."}
+                <div id="listsMenuMyListsBigDiv">
+                    {lists!==null?lists.value.map((value,index)=>{if(index>0){return(<div key={index+0.5}><img className="myListsImages"src="https://image.shutterstock.com/image-vector/modern-flat-sliders-icon-symbol-600w-2108399819.jpg"alt="list" /><h4 key={index}>{value.displayName}</h4></div>);}}):"loading..."}
                 </div>
                 <div id="listsMenuNewListDiv">
                     <img id="listsMenuNewListImage"src="https://image.shutterstock.com/image-vector/colored-plus-symbol-cross-icon-600w-494267107.jpg"alt="text" />
