6faf91eda7a300112ff26aabb2c57ba2b6d57a85
burggste000 <steedsranch@gmail.com>
2022-06-07 10:51:53 -0600

    LIST IMAGES DARKEN WHEN LIST IS CLICKED

--- src/components/SteedsTasksComponents/ListsMenu/ListsMenu.jsx
+++ src/components/SteedsTasksComponents/ListsMenu/ListsMenu.jsx
@@ -116,25 +116,24 @@ const ListsMenu=props=>{
                 <img id="listsMenuButton"src="https://image.shutterstock.com/image-vector/menu-icon-trendy-flat-style-600w-1350292571.jpg"alt="text"onClick={()=>props.setListsMenuClicked(!props.listsMenuClicked)} />
             </div>
             <div id="listsScrollDiv">
-            {/*Working here*/}
                 <div id={props.currentList!=="My Day"?"listsMenuDayDiv":"listsMenuDayDivDark"}onClick={clickedListDiv}>
-                    <img id="listsMenuDayImage"src="https://image.shutterstock.com/image-vector/sun-vector-icon-modern-design-600w-1415031134.jpg"alt="text"onClick={clickedListImg} />
+                    <img id={props.currentList!=="My Day"?"listsMenuDayImage":"listsMenuDayImageDark"}src="https://image.shutterstock.com/image-vector/sun-vector-icon-modern-design-600w-1415031134.jpg"alt="text"onClick={clickedListImg} />
                     <h4 id="listsMenuDayText"onClick={clickedListText}>My Day</h4>
                 </div>
                 <div id={props.currentList!=="Assigned To Me"?"listsMenuAssignedDiv":"listsMenuAssignedDivDark"}onClick={clickedListDiv}>
-                    <img id="listsMenuAssignedImage"src="https://image.shutterstock.com/image-vector/flat-portrait-black-man-icon-600w-2134341081.jpg"alt="text"onClick={clickedListImg} />
+                    <img id={props.currentList!=="Assigned To Me"?"listsMenuAssignedImage":"listsMenuAssignedImageDark"}src="https://image.shutterstock.com/image-vector/flat-portrait-black-man-icon-600w-2134341081.jpg"alt="text"onClick={clickedListImg} />
                     <h4 id="listsMenuAssignedText"onClick={clickedListText}>Assigned To Me</h4>
                 </div>
                 <div id={props.currentList!=="Flagged email"?"listsMenuFlaggedDiv":"listsMenuFlaggedDivDark"}onClick={clickedListDiv}>
-                    <img id="listsMenuFlaggedImage"src="https://image.shutterstock.com/image-vector/line-icon-flag-600w-654361600.jpg"alt="text"onClick={clickedListImg} />
+                    <img id={props.currentList!=="Flagged email"?"listsMenuFlaggedImage":"listsMenuFlaggedImageDark"}src="https://image.shutterstock.com/image-vector/line-icon-flag-600w-654361600.jpg"alt="text"onClick={clickedListImg} />
                     <h4 id="listsMenuFlaggedText"onClick={clickedListText}>Flagged email</h4>
                 </div>
                 <div id={props.currentList!=="Tasks"?"listsMenuTasksDiv":"listsMenuTasksDivDark"}onClick={clickedListDiv}>
-                    <img id="listsMenuTasksImage"src="https://image.shutterstock.com/image-vector/home-icon-trendy-flat-style-600w-675381382.jpg"alt="text"onClick={clickedListImg} />
+                    <img id={props.currentList!=="Tasks"?"listsMenuTasksImage":"listsMenuTasksImageDark"}src="https://image.shutterstock.com/image-vector/home-icon-trendy-flat-style-600w-675381382.jpg"alt="text"onClick={clickedListImg} />
                     <h4 id="listsMenuTasksText"onClick={clickedListText}>{lists!==null?lists.value[0].displayName:"loading lists"}</h4>
                 </div>
                 <div id="listsMenuMyListsBigDiv">
-                    {lists!==null?lists.value.map((value,index)=>{if(index>0){return(<div className={props.currentList!==value.displayName?"myListsDiv":"myListsDivDark"}key={index+0.5}onClick={clickedListDiv}><img className="myListsImages"src="https://image.shutterstock.com/image-vector/modern-flat-sliders-icon-symbol-600w-2108399819.jpg"alt="list"onClick={clickedListImg} /><h4 className="myListsText"onClick={clickedListText}key={index}>{value.displayName}</h4></div>);}}):"loading..."}
+                    {lists!==null?lists.value.map((value,index)=>{if(index>0){return(<div className={props.currentList!==value.displayName?"myListsDiv":"myListsDivDark"}key={index+0.5}onClick={clickedListDiv}><img className={props.currentList!==value.displayName?"myListsImages":"myListsImagesDark"}src="https://image.shutterstock.com/image-vector/modern-flat-sliders-icon-symbol-600w-2108399819.jpg"alt="list"onClick={clickedListImg} /><h4 className="myListsText"onClick={clickedListText}key={index}>{value.displayName}</h4></div>);}}):"loading..."}
                     <div id="listsMenuNewListDiv">
                         <img id="listsMenuNewListImage"src="https://image.shutterstock.com/image-vector/colored-plus-symbol-cross-icon-600w-494267107.jpg"alt="text" />
                         <form onSubmit={e=>{e.preventDefault();createList(newList);setNewList('');}}>
