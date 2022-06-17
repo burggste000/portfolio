698f27348fc8bf7876474bbb2efee7abc3a17e0f
burggste000 <steedsranch@gmail.com>
2022-06-17 09:18:54 -0600

    GETTING BETTER LITTLE BY LITTLE

--- src/components/SteedsTasksComponents/SuggestionsMenu/SuggestionsMenu.jsx
+++ src/components/SteedsTasksComponents/SuggestionsMenu/SuggestionsMenu.jsx
@@ -7,7 +7,7 @@ const SuggestionsMenu=props=>{
     return(
         <div id={props.suggestionsClicked===false?"hide":"suggestionsParentDiv"}>
                 <h2 id="SuggestionsTitle">Suggestions</h2>
-            <div id="suggestionsXdiv">
+            <div id="suggestionsXdiv"onMouseEnter={()=>setSuggestionsMenuXhovered(true)}onMouseLeave={()=>setSuggestionsMenuXhovered(false)}>
                 <img id={suggestionsMenuXhovered===false?"suggestionsX":"darkSuggestionsX"}onClick={()=>props.setSuggestionsClicked(false)}src="https://image.shutterstock.com/image-vector/cancel-cross-close-icon-vector-600w-294801173.jpg"alt="close button" />
             </div>
         </div>
