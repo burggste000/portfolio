7286c1b1a9a55d7ac250c1441f813de27b5a6525
burggste000 <steedsranch@gmail.com>
2022-09-23 19:03:05 -0600

    ADDED NOTE TO SUGGESTIONS MENU

--- src/components/SteedsTasksComponents/SuggestionsMenu/SuggestionsMenu.jsx
+++ src/components/SteedsTasksComponents/SuggestionsMenu/SuggestionsMenu.jsx
@@ -57,6 +57,7 @@ const SuggestionsMenu=props=>{
                 <h2 id="SuggestionsTitle">Suggestions</h2>
             <div id="suggestionsXdiv"onMouseEnter={()=>setSuggestionsMenuXhovered(true)}onMouseLeave={()=>setSuggestionsMenuXhovered(false)}>
                 <img id={suggestionsMenuXhovered===false?"suggestionsX":"darkSuggestionsX"}onClick={()=>props.setSuggestionsClicked(false)}src="https://image.shutterstock.com/image-vector/cancel-cross-close-icon-vector-600w-294801173.jpg"alt="close button" />
+                <p><i>Personalized suggestions are comming soon!!</i></p>
             </div>
         </div>
     );
