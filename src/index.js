d760ebeecddcb8427ce7bae0897e6ba2bcc36e6c
burggste000 <steedsranch@gmail.com>
2022-09-24 10:47:37 -0600

    downloaded react router and wrapped app.js in it

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "./authConfig";
+import{BrowserRouter}from"react-router-dom";
 
 const msalInstance = new PublicClientApplication(msalConfig);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <MsalProvider instance={msalInstance}>
-            <App />
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
         </MsalProvider>
     </React.StrictMode>
 );
