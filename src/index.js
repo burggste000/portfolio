325bf5e9e8e1a30f099626f61a8fdbe270417d81
burggste000 <steedsranch@gmail.com>
2022-09-24 14:54:13 -0600

    FIXING REACT ROUTING

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <MsalProvider instance={msalInstance}>
-            <BrowserRouter>
+            <BrowserRouter basename="/task-integration">
                 <App />
             </BrowserRouter>
         </MsalProvider>
