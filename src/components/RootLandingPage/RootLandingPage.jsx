c84af9a9987ce2e4032f28ca2026141a8a84f7ca
burggste000 <steedsranch@gmail.com>
2022-10-05 22:24:12 -0600

    PORTFOLIO ONLY SHOWS WHEN STATE IS SET TO VIEW

--- src/components/RootLandingPage/RootLandingPage.jsx
+++ src/components/RootLandingPage/RootLandingPage.jsx
@@ -1,9 +1,12 @@
 import"./rootLandingPage.css";
 import{TheMiddle}from"./TheMiddle/TheMiddle.jsx";
+import react from"react";
+
+const[viewClicked,setViewClicked]=react.useState(false);
 
 const RootLandingPage=()=>{
     return(
-        <TheMiddle />
+        <TheMiddle viewClicked={viewClicked} />
     );
 };
 export{RootLandingPage};
--- src/components/RootLandingPage/TheMiddle/TheMiddle.jsx
+++ src/components/RootLandingPage/TheMiddle/TheMiddle.jsx
@@ -3,14 +3,14 @@ import{Projects}from"./Projects/Projects.jsx";
 import{Skills}from"./Skills/Skills.jsx";
 import{About}from"./About/About.jsx";
 
-const TheMiddle=()=>{
+const TheMiddle=props=>{
     return(
-        <>
+        <div id={props.viewClicked===true?'':"hide"}>
             <p id="welcome">Hi, I'm Stephen</p>
             <Projects />
             <About />
             <Skills />
-        </>
+        </div>
     );
 };
 export{TheMiddle};
