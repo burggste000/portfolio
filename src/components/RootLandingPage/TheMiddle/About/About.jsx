d6606e27575d6b46e497ae9c0776914f783b91b2
burggste000 <steedsranch@gmail.com>
2022-10-05 21:12:55 -0600

    CREATED ABOUT DIVISION

---
+++ src/components/RootLandingPage/TheMiddle/About/About.jsx
@@ -0,0 +1,10 @@
+import"./about.css";
+
+const About=()=>{
+    return(
+        <div id="aboutDiv">
+            <p>Jon Duckett's book titled, HTML and CSS Design and Build Websites, is what got me hooked on the dream of being a Front-End Web Developer. I loved learning how to make the seemingly, "magical and mysterious", visual website effects! I spent all of my free-time and weekends problem solving and learning about JavaScript, and applying what I learned on small projects. After eight months had passed, I started feeling comfortable with many of the implementations that HTML, CSS, and JavaScript play part of in the web development process. I took an offer from a senior mentor in the Software Engineering industry and became his full-time student for two months. Those two months were a full-time one-on-one class creating projects in C++ and utilizing Linux and command line Git for version control. This was a key foundation in my education, because I got to use and grow my logical thinking and problem solving with manipulating differing types of data. After my two month mentorship on C++, I started a Devmountain bootcamp specializing as a Full-Stack Web Developer using JavaScript, ReactJS, CSS, NodeJS, ExpressJS, SQL, and many other advancing technologies. After graduating the Devmounain Bootcamp, I have continued building Full-Stack web applications focusing on Front-End Web Development. I love problem solving and contributing to the world of Front-End Web Development!</p>
+        </div>
+    );
+};
+export{About};
--- src/components/RootLandingPage/TheMiddle/TheMiddle.jsx
+++ src/components/RootLandingPage/TheMiddle/TheMiddle.jsx
@@ -1,12 +1,14 @@
 import"./theMiddle.css";
 import{Projects}from"./Projects/Projects.jsx";
 import{Skills}from"./Skills/Skills.jsx";
+import{About}from"./About/About.jsx";
 
 const TheMiddle=()=>{
     return(
         <>
             <p id="welcome">Hi, I'm Stephen</p>
             <Projects />
+            <About />
             <Skills />
         </>
     );
