9c3d45cf8243cdb00a695d9e49c0364bbe06278e
burggste000 <steedsranch@gmail.com>
2022-10-08 19:36:06 -0600

    ADDED VIDEO WHEN SOMEONE CLICKS VIEW

--- src/components/RootLandingPage/Start/Start.jsx
+++ src/components/RootLandingPage/Start/Start.jsx
@@ -1,10 +1,25 @@
 import"./start.css";
+import react from"react";
+import introductionVideo from"./introductionVideo.webm";
 const Start=props=>{
+    const[showIntro,setShowIntro]=react.useState(false);
+
+    function startIntro(){
+        const timer=setTimeout(()=>{
+            setShowIntro(false);
+        },20000);
+    }
+
     return(
-        <div id="viewDiv">
-            <p id="welcome">Hi, I'm Stephen</p>
-            <button id="startButton"onClick={()=>props.setViewClicked(true)}>View</button>
-        </div>
+        <>
+            <div id="viewDiv">
+                <p id="welcome">Hi, I'm Stephen</p>
+                <button id="startButton"onClick={()=>{setShowIntro(true);props.setViewClicked(true);startIntro();}}>View</button>
+            </div>
+            <video autoPlay loop muted id={showIntro===false?"hide":"introVideo"}>
+                <source src={introductionVideo}type="video/webm" />
+            </video>
+        </>
     );
 };
 export{Start};
