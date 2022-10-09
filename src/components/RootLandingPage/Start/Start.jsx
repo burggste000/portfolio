15ce49749b8c2a71ed6c7efb578c40ae49d7d255
burggste000 <steedsranch@gmail.com>
2022-10-08 20:04:44 -0600

    REVERTING BACK TO BEFORE LAST COMMIT

--- src/components/RootLandingPage/Start/Start.jsx
+++ src/components/RootLandingPage/Start/Start.jsx
@@ -1,25 +1,10 @@
 import"./start.css";
-import react from"react";
-import introductionVideo from"./introductionVideo.webm";
 const Start=props=>{
-    const[showIntro,setShowIntro]=react.useState(false);
-
-    function startIntro(){
-        const timer=setTimeout(()=>{
-            setShowIntro(false);
-        },20000);
-    }
-
     return(
-        <>
-            <div id="viewDiv">
-                <p id="welcome">Hi, I'm Stephen</p>
-                <button id="startButton"onClick={()=>{setShowIntro(true);props.setViewClicked(true);startIntro();}}>View</button>
-            </div>
-            <video autoPlay loop muted id={showIntro===false?"hide":"introVideo"}>
-                <source src={introductionVideo}type="video/webm" />
-            </video>
-        </>
+        <div id="viewDiv">
+            <p id="welcome">Hi, I'm Stephen</p>
+            <button id={props.viewClicked===true?"hide":"startButton"}onClick={()=>props.setViewClicked(true)}>View</button>
+        </div>
     );
 };
 export{Start};
