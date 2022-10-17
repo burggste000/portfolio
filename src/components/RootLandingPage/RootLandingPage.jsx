bd0be25d1b74f7852068d7ad7012aebaef132e42
burggste000 <steedsranch@gmail.com>
2022-10-17 13:08:17 -0600

    VIDEO SHOWS FOR 19 SECONDS AFTER VIEW IS CLICKED, THEN MY PORTFOLIO PAGE SHOWS

--- src/components/RootLandingPage/RootLandingPage.jsx
+++ src/components/RootLandingPage/RootLandingPage.jsx
@@ -1,14 +1,17 @@
 import"./rootLandingPage.css";
 import{TheMiddle}from"./TheMiddle/TheMiddle.jsx";
 import{Start}from"./Start/Start.jsx";
+import{TransitionVideo}from"./Start/TransitionVideo/TransitionVideo.jsx";
 import react from"react";
 
 
 const RootLandingPage=()=>{
     const[viewClicked,setViewClicked]=react.useState(false);
+    const[introVideoShowing,setIntroVideoShowing]=react.useState(false);
     return(
         <>
-            <Start viewClicked={viewClicked}setViewClicked={setViewClicked} />
+            <Start viewClicked={viewClicked}setViewClicked={setViewClicked}setIntroVideoShowing={setIntroVideoShowing} />
+            <TransitionVideo viewClicked={viewClicked}introVideoShowing={introVideoShowing} />
             <TheMiddle viewClicked={viewClicked} />
         </>
     );
--- src/components/RootLandingPage/Start/Start.jsx
+++ src/components/RootLandingPage/Start/Start.jsx
@@ -1,9 +1,16 @@
 import"./start.css";
 const Start=props=>{
+
+    function showIntroVideo(){
+        setTimeout(()=>{
+            props.setIntroVideoShowing(false);
+        },18000);
+    }
+
     return(
         <div id="viewDiv">
             <p id="welcome">Hi, I'm Stephen</p>
-            <button id={props.viewClicked===true?"hide":"startButton"}onClick={()=>props.setViewClicked(true)}>View</button>
+            <button id={props.viewClicked===true?"hide":"startButton"}onClick={()=>{props.setViewClicked(true);props.setIntroVideoShowing(true);showIntroVideo();}}>View</button>
         </div>
     );
 };
