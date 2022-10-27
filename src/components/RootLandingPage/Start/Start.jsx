1c8d79ae34318d52c0a82d857ccc0e365e55f16c
burggste000 <steedsranch@gmail.com>
2022-10-27 14:38:04 -0600

    ADDED BACKGROUND IMAGE AND COLOR TO VIEW BUTTON

--- src/components/RootLandingPage/Start/Start.jsx
+++ src/components/RootLandingPage/Start/Start.jsx
@@ -1,26 +1,11 @@
 import"./start.css";
 
 const Start=props=>{
-
-    // function showIntroVideo(){
-    //     setTimeout(()=>{
-    //         props.setIntroVideoShowing(false);
-    //     },18000);
-    // }
-
     return(
-        <>
-            {/* <div>
-                <video id={props.viewClicked===true?"hide":"startBackgroundVid"}autoPlay="true"loop="true"muted="true">
-                    <source src="https://ak.picdn.net/shutterstock/videos/1082922796/preview/stock-footage-mirror-ball-disco-lights-star-glitter-club-dance-party-background.webm"type="video/webm" />
-                </video>
-            </div> */}
-            
-            <div id={props.viewClicked===true?"hide":"interactDiv"}>
-                <p id={props.viewClicked===true?"hide":"welcome"}>Hi, I'm Stephen</p>
-                <button id={props.viewClicked===true?"hide":"startButton"}onClick={()=>props.setViewClicked(true)}>View</button>
-            </div>
-        </>
+        <div id={props.viewClicked===true?"hide":"interactDiv"}>
+            <p id={props.viewClicked===true?"hide":"welcome"}>Hi, I'm Stephen</p>
+            <button id={props.viewClicked===true?"hide":"startButton"}onClick={()=>props.setViewClicked(true)}>View</button>
+        </div>
     );
 };
 export{Start};
