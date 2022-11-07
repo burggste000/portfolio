d7c53946b028a0c91893086ba12c9a55a5fa3dbe
burggste000 <steedsranch@gmail.com>
2022-11-07 12:41:29 -0700

    UPDATED THE PICTURE OF ME

--- src/components/RootLandingPage/TheMiddle/Projects/Projects.jsx
+++ src/components/RootLandingPage/TheMiddle/Projects/Projects.jsx
@@ -1,28 +1,40 @@
-import"./projects.css"
+import "./projects.css";
+import mePhoto from "./mePhoto.JPG";
 
-const Projects=()=>{
-    return(
-        <div id="background1">
-
-            <div id="myPicDiv">
-                <img id="me"src="https://avatars.githubusercontent.com/u/88006165?s=400&u=29c195b17d259548806350d632fc4f20d67c8aac&v=4"alt="me" />
-            </div>
-            <h2>My websites and projects</h2>
-            <div id="projectsDiv">
-                <figure className="projectLabels">
-                    <img className="projectImages"src="https://images.squarespace-cdn.com/content/v1/62bbaaef7cc39c494bcaba86/8eab7f38-b20d-4bc5-9f5a-bf99f770c6cc/Untitled+1517+%281%29.png?format=1500w"alt="company website project" />
-                    <figcaption>Fishing Bridge USA Website</figcaption>
-                </figure>
-                <figure className="projectLabels">
-                    <img className="projectImages"src="https://cdn.pixabay.com/photo/2017/09/29/00/30/checkmark-icon-2797531_960_720.png"alt="task project" />
-                    <figcaption>Task Management Website</figcaption>
-                </figure>
-                <figure className="projectLabels" >
-                    <img className="projectImages"src="https://live.staticflickr.com/8536/10187142295_8a12343d8e_n.jpg"alt="organization project" />
-                    <figcaption>Organization Chart (Demo)</figcaption>
-                </figure>
-            </div>
-        </div>
-    );
+const Projects = () => {
+  return (
+    <div id="background1">
+      <div id="myPicDiv">
+        <img id="me" src={mePhoto} alt="me" />
+      </div>
+      <h2>My websites and projects</h2>
+      <div id="projectsDiv">
+        <figure className="projectLabels">
+          <img
+            className="projectImages"
+            src="https://images.squarespace-cdn.com/content/v1/62bbaaef7cc39c494bcaba86/8eab7f38-b20d-4bc5-9f5a-bf99f770c6cc/Untitled+1517+%281%29.png?format=1500w"
+            alt="company website project"
+          />
+          <figcaption>Fishing Bridge USA Website</figcaption>
+        </figure>
+        <figure className="projectLabels">
+          <img
+            className="projectImages"
+            src="https://cdn.pixabay.com/photo/2017/09/29/00/30/checkmark-icon-2797531_960_720.png"
+            alt="task project"
+          />
+          <figcaption>Task Management Website</figcaption>
+        </figure>
+        <figure className="projectLabels">
+          <img
+            className="projectImages"
+            src="https://live.staticflickr.com/8536/10187142295_8a12343d8e_n.jpg"
+            alt="organization project"
+          />
+          <figcaption>Organization Chart (Demo)</figcaption>
+        </figure>
+      </div>
+    </div>
+  );
 };
-export{Projects};
+export { Projects };
--- src/components/RootLandingPage/TheMiddle/TheMiddle.jsx
+++ src/components/RootLandingPage/TheMiddle/TheMiddle.jsx
@@ -1,15 +1,15 @@
-import"./theMiddle.css";
-import{Projects}from"./Projects/Projects.jsx";
-import{Skills}from"./Skills/Skills.jsx";
-import{About}from"./About/About.jsx";
+import "./theMiddle.css";
+import { Projects } from "./Projects/Projects.jsx";
+import { Skills } from "./Skills/Skills.jsx";
+import { About } from "./About/About.jsx";
 
-const TheMiddle=props=>{
-    return(
-        <div id={props.viewClicked===true?'':"hide"}>
-            <Projects />
-            {/* <About /> */}
-            {/* <Skills /> */}
-        </div>
-    );
+const TheMiddle = (props) => {
+  return (
+    <div id={props.viewClicked === true ? "" : "hide"}>
+      <Projects />
+      {/* <About /> */}
+      {/* <Skills /> */}
+    </div>
+  );
 };
-export{TheMiddle};
+export { TheMiddle };
