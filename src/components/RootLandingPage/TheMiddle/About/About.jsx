082f28ea7ae54f9c1779f310727e4f1f6ea795d5
burggste000 <steedsranch@gmail.com>
2022-11-07 15:50:21 -0700

    DIVIDED MY ABOUT ME SECTION INTO MORE PARAGRAPHS AND ADJUSTED THE DIVISION'S MAX-WIDTH

--- src/components/RootLandingPage/TheMiddle/About/About.jsx
+++ src/components/RootLandingPage/TheMiddle/About/About.jsx
@@ -3,7 +3,7 @@ import "./about.css";
 const About = () => {
   return (
     <div id="aboutDiv">
-      <p id="aboutParagraph">
+      <p className="aboutParagraph">
         Jon Duckett's book titled, HTML and CSS Design and Build Websites, is
         what got me hooked on the dream of being a Front-End Web Developer. I
         loved learning how to make the seemingly, "magical and mysterious",
@@ -11,19 +11,24 @@ const About = () => {
         solving and learning about JavaScript, and applying what I learned on
         small projects. After eight months had passed, I started feeling
         comfortable with many of the implementations that HTML, CSS, and
-        JavaScript play part of in the web development process. I took an offer
-        from a senior mentor in the Software Engineering industry and became his
-        full-time student for two months. Those two months were a full-time
-        one-on-one class creating projects in C++ and utilizing Linux and
-        command line Git for version control. This was a key foundation in my
-        education, because I got to use and grow my logical thinking and problem
-        solving with manipulating differing types of data. After my two month
-        mentorship on C++, I started a Devmountain bootcamp specializing as a
-        Full-Stack Web Developer using JavaScript, ReactJS, CSS, NodeJS,
-        ExpressJS, SQL, and many other advancing technologies. After graduating
-        the Devmounain Bootcamp, I have continued building Full-Stack web
-        applications focusing on Front-End Web Development. I love problem
-        solving and contributing to the world of Front-End Web Development!
+        JavaScript play part of in the web development process.
+      </p>
+      <p className="aboutParagraph">
+        I took an offer from a senior mentor in the Software Engineering
+        industry and became his full-time student for two months. Those two
+        months were a full-time one-on-one class creating projects in C++ and
+        utilizing Linux and command line Git for version control. This was a key
+        foundation in my education, because I got to use and grow my logical
+        thinking and problem solving with manipulating differing types of data.
+      </p>
+      <p className="aboutParagraph">
+        After my two month mentorship on C++, I started a Devmountain bootcamp
+        specializing as a Full-Stack Web Developer using JavaScript, ReactJS,
+        CSS, NodeJS, ExpressJS, SQL, and many other advancing technologies.
+        After graduating the Devmounain Bootcamp, I have continued building
+        Full-Stack web applications focusing on Front-End Web Development. I
+        love problem solving and contributing to the world of Front-End Web
+        Development!
       </p>
       <p>Email me at: steedsranch@gmail.com</p>
       <div id="aboutLinksDiv">
