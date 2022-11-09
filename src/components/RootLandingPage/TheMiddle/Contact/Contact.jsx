6bdf56f3335aa28673066c357f8497d8ceb0bb51
burggste000 <steedsranch@gmail.com>
2022-11-09 15:31:13 -0700

    EVENLY SPACED CONTACT LINKS

--- src/components/RootLandingPage/TheMiddle/Contact/Contact.jsx
+++ src/components/RootLandingPage/TheMiddle/Contact/Contact.jsx
@@ -5,9 +5,14 @@ const Contact = () => {
 		<div id="contactMeDiv">
 			<h3 id="contactTitle">Reach out!</h3>
 			<div id="aboutLinksDiv">
-				<a href="https://github.com/burggste000">My GitHub Profile</a>
+				<a className="contactLinks" href="https://github.com/burggste000">
+					My GitHub Profile
+				</a>
 				<p id="email">Email me at: steedsranch@gmail.com</p>
-				<a href="https://www.linkedin.com/in/stephen-burggraaf-867993226/">
+				<a
+					className="contactLinks"
+					href="https://www.linkedin.com/in/stephen-burggraaf-867993226/"
+				>
 					My LinkedIn Profile
 				</a>
 			</div>
