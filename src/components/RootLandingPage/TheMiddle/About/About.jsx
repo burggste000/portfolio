64a6c29f451476f00bbe7a94622ef5c8f1cdf3df
burggste000 <steedsranch@gmail.com>
2022-11-08 20:49:04 -0700

    NEW ENDPOINT WORKS

--- src/components/RootLandingPage/TheMiddle/About/About.jsx
+++ src/components/RootLandingPage/TheMiddle/About/About.jsx
@@ -1,43 +1,43 @@
 import "./about.css";
 
 const About = () => {
-  return (
-    <div id="aboutDiv">
-      <p className="aboutParagraph">
-        Jon Duckett's book titled, HTML and CSS Design and Build Websites, is
-        what got me hooked on the dream of being a Front-End Web Developer. I
-        loved learning how to make the seemingly, "magical and mysterious",
-        visual website effects! I spent all of my free-time and weekends problem
-        solving and learning about JavaScript, and applying what I learned on
-        small projects. After eight months had passed, I started feeling
-        comfortable with many of the implementations that HTML, CSS, and
-        JavaScript play part of in the web development process.
-      </p>
-      <p className="aboutParagraph">
-        I took an offer from a senior mentor in the Software Engineering
-        industry and became his full-time student for two months. Those two
-        months were a full-time one-on-one class creating projects in C++ and
-        utilizing Linux and command line Git for version control. This was a key
-        foundation in my education, because I got to use and grow my logical
-        thinking and problem solving with manipulating differing types of data.
-      </p>
-      <p className="aboutParagraph">
-        After my two month mentorship on C++, I started a Devmountain bootcamp
-        specializing as a Full-Stack Web Developer using JavaScript, ReactJS,
-        CSS, NodeJS, ExpressJS, SQL, and many other advancing technologies.
-        After graduating the Devmounain Bootcamp, I have continued building
-        Full-Stack web applications focusing on Front-End Web Development. I
-        love problem solving and contributing to the world of Front-End Web
-        Development!
-      </p>
-      <p>Email me at: steedsranch@gmail.com</p>
-      <div id="aboutLinksDiv">
-        <a href="https://github.com/burggste000">My GitHub Profile</a>
-        <a href="https://www.linkedin.com/in/stephen-burggraaf-867993226/">
-          My LinkedIn Profile
-        </a>
-      </div>
-    </div>
-  );
+	return (
+		<div id="aboutDiv">
+			<p className="aboutParagraph">
+				Jon Duckett's book titled, HTML and CSS Design and Build Websites, is
+				what got me hooked on the dream of being a Front-End Web Developer. I
+				loved learning how to make the seemingly, "magical and mysterious",
+				visual website effects! I spent all of my free-time and weekends problem
+				solving and learning about JavaScript, and applying what I learned on
+				small projects. After eight months had passed, I started feeling
+				comfortable with many of the implementations that HTML, CSS, and
+				JavaScript play part of in the web development process.
+			</p>
+			<p className="aboutParagraph">
+				I took an offer from a senior mentor in the Software Engineering
+				industry and became his full-time student for two months. Those two
+				months were a full-time one-on-one class creating projects in C++ and
+				utilizing Linux and command line Git for version control. This was a key
+				foundation in my education, because I got to use and grow my logical
+				thinking and problem solving with manipulating differing types of data.
+			</p>
+			<p className="aboutParagraph">
+				After my two month mentorship on C++, I started a Devmountain bootcamp
+				specializing as a Full-Stack Web Developer using JavaScript, ReactJS,
+				CSS, NodeJS, ExpressJS, SQL, and many other advancing technologies.
+				After graduating the Devmounain Bootcamp, I have continued building
+				Full-Stack web applications focusing on Front-End Web Development. I
+				love problem solving and contributing to the world of Front-End Web
+				Development!
+			</p>
+			<p>Email me at: steedsranch@gmail.com</p>
+			<div id="aboutLinksDiv">
+				<a href="https://github.com/burggste000">My GitHub Profile</a>
+				<a href="https://www.linkedin.com/in/stephen-burggraaf-867993226/">
+					My LinkedIn Profile
+				</a>
+			</div>
+		</div>
+	);
 };
 export { About };
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App/App.js';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App/App.js";
+import reportWebVitals from "./reportWebVitals";
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "./authConfig";
-import{BrowserRouter}from"react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const msalInstance = new PublicClientApplication(msalConfig);
-const root=ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <MsalProvider instance={msalInstance}>
-            <BrowserRouter basename="/task-integration">
-                <App />
-            </BrowserRouter>
-        </MsalProvider>
-    </React.StrictMode>
+	<React.StrictMode>
+		<MsalProvider instance={msalInstance}>
+			<BrowserRouter basename="/portfolio">
+				<App />
+			</BrowserRouter>
+		</MsalProvider>
+	</React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
