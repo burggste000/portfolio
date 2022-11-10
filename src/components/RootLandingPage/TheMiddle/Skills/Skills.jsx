bcfc7603cf94326ed457fbc733d1f30b886d2935
burggste000 <steedsranch@gmail.com>
2022-11-10 13:24:42 -0700

    spaced skills and put a new background for the skills division

--- src/components/RootLandingPage/TheMiddle/Skills/Skills.jsx
+++ src/components/RootLandingPage/TheMiddle/Skills/Skills.jsx
@@ -1,27 +1,46 @@
-import"./skills.css";
-const Skills=()=>{
-    return(
-        <div id="background2">
-            <div id="skillsTitleDiv">
-                <h2 id="skillsTitle">My Skills</h2>
-            </div>
-            <div className="skillsDivs">
-                <img id="javascriptPic"className="skills"src="https://live.staticflickr.com/3708/14102205140_d25a5588ea_m.jpg"alt="javascript" />
-                <img id="reactPic"className="skills"src="https://live.staticflickr.com/4779/25850433237_f57ce6b815_m.jpg"alt="reactjs" />
-                <img id="htmlPic"className="skills"src="https://image.shutterstock.com/image-vector/logo-vector-html-5-low-600w-1601607721.jpg"alt="html" />
-            </div>
-            <div className="skillsDivs">
-                <img id="cssPic"className="skills"src="https://image.shutterstock.com/image-vector/logo-vector-css-3-low-600w-1902943426.jpg"alt="css" />
-                <img id="githubPic"className="skills"src="https://live.staticflickr.com/8328/28828372394_f6260987d9_s.jpg"alt="github" />
-                <img id="linuxPic"className="skills"src="https://live.staticflickr.com/8357/8340340399_c4df9c32dd_n.jpg"alt="linux" />
-                <img id="nodePic"className="skills"src="https://live.staticflickr.com/65535/50886824773_38ba7914dd_w.jpg"alt="node" />
-            </div>
-            <div className="skillsDivs">
-                <img id="expressPic"className="skills"src="https://live.staticflickr.com/65535/50992256612_6e005d1695_w.jpg"alt="express" />
-                <img id="figmaPic"className="skills"src="https://image.shutterstock.com/image-vector/colorful-app-logo-on-dark-600w-2001876035.jpg"alt="figma" />
-                <img id="qaPic"className="skills"src="https://image.shutterstock.com/image-vector/qa-logo-design-vector-template-600w-2171017265.jpg"alt="qa testing" />
-            </div>
-        </div>
-    );
+import "./skills.css";
+import javaScriptLogo from "./images/javaScript.jpg";
+import reactLogo from "./images/react.jpg";
+import htmlLogo from "./images/html.jpg";
+import cssLogo from "./images/css.jpg";
+import gitHubLogo from "./images/gitHub.jpg";
+import linuxLogo from "./images/linux.jpg";
+import nodeLogo from "./images/node.png";
+import microsoftWindowsLogo from "./images/microsoftWindows.png";
+import figmaLogo from "./images/figma.jpg";
+import visualStudioLogo from "./images/visualStudio.png";
+
+const Skills = () => {
+	return (
+		<div id="background2">
+			<div id="skillsTitleDiv">
+				<h2 id="skillsTitle">My Skills</h2>
+			</div>
+			<div className="skillsDivs">
+				<img className="skills" src={javaScriptLogo} alt="javascript" />
+				<img className="skills" src={reactLogo} alt="reactjs" />
+				<img className="skills" src={cssLogo} alt="css" />
+			</div>
+			<div className="skillsDivs">
+				<img className="skills" src={gitHubLogo} alt="github" />
+				<img id="htmlLogo" className="skills" src={htmlLogo} alt="html" />
+				<img className="skills" src={nodeLogo} alt="node" />
+				<img className="skills" src={linuxLogo} alt="linux" />
+			</div>
+			<div className="skillsDivs">
+				<img
+					className="skills"
+					src={visualStudioLogo}
+					alt="visual studio code"
+				/>
+				<img
+					className="skills"
+					src={microsoftWindowsLogo}
+					alt="microsoft windows"
+				/>
+				<img id="figmaLogo" className="skills" src={figmaLogo} alt="figma" />
+			</div>
+		</div>
+	);
 };
-export{Skills};
+export { Skills };
